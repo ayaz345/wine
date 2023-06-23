a7cf2133e76daba6dbe43211316e0f543f7d498f
Sourcery AI <>
2023-06-23 09:55:29 +0000

    'Refactored by Sourcery'

--- tools/gdbinit.py
+++ tools/gdbinit.py
@@ -36,14 +36,14 @@ class LoadSymbolFiles(gdb.Command):
 
   def invoke(self, arg, from_tty):
     pid = gdb.selected_inferior().pid
-    if not pid in self.libs: self.libs[pid] = {}
+    if pid not in self.libs: self.libs[pid] = {}
 
     def command(cmd, confirm=from_tty, to_string=not from_tty):
       gdb.execute(cmd, from_tty=confirm, to_string=to_string)
 
     def execute(cmd):
       return subprocess.check_output(cmd, stderr=subprocess.STDOUT) \
-                       .decode('utf-8')
+                         .decode('utf-8')
 
     # load mappings addresses
     libs = {}
@@ -63,51 +63,50 @@ class LoadSymbolFiles(gdb.Command):
 
     # load symbol file for new mappings
     for k in set(libs) - set(self.libs[pid]):
-        if arg is not None and re.search(arg, k) is None: continue
-        addr = self.libs[pid][k] = libs[k]
-        has_debug = False
-        offs = None
+      if arg is not None and re.search(arg, k) is None: continue
+      addr = self.libs[pid][k] = libs[k]
+      has_debug = False
+      offs = None
 
-        try:
-          out = execute(['file', k])
-        except:
-          continue
+      try:
+        out = execute(['file', k])
+      except:
+        continue
 
         # try loading mapping as ELF
-        try:
-          out = execute(['readelf', '-l', k])
-          for line in out.split('\n'):
-            if not 'LOAD' in line: continue
-            base = int(line.split()[2], 16)
-            break
-        except:
-          # assume mapping is PE
-          base = -1
+      try:
+        out = execute(['readelf', '-l', k])
+        for line in out.split('\n'):
+          if 'LOAD' not in line: continue
+          base = int(line.split()[2], 16)
+          break
+      except:
+        # assume mapping is PE
+        base = -1
 
-        try:
-          name = None
-          cmd = 'add-symbol-file "{}"'.format(k)
-          out = execute(['objdump', '-h', k])
-          for line in out.split('\n'):
-            if '2**' in line:
-              _, name, _, vma, _, off, _ = line.split(maxsplit=6)
-              if base < 0: offs = int(off, 16)
-              else: offs = int(vma, 16) - base
-            if 'ALLOC' in line:
-              cmd += ' -s {} 0x{:x}'.format(name, addr + offs)
-            elif name in ['.gnu_debuglink', '.debug_info']:
-              has_debug = True
-            elif 'DEBUGGING' in line:
-              has_debug = True
-        except:
-          continue
+      try:
+        name = None
+        cmd = 'add-symbol-file "{}"'.format(k)
+        out = execute(['objdump', '-h', k])
+        for line in out.split('\n'):
+          if '2**' in line:
+            _, name, _, vma, _, off, _ = line.split(maxsplit=6)
+            offs = int(off, 16) if base < 0 else int(vma, 16) - base
+          if 'ALLOC' in line:
+            cmd += ' -s {} 0x{:x}'.format(name, addr + offs)
+          elif name in ['.gnu_debuglink', '.debug_info']:
+            has_debug = True
+          elif 'DEBUGGING' in line:
+            has_debug = True
+      except:
+        continue
 
-        if not has_debug:
-          print('no debugging info found in {}'.format(k))
-          continue
+      if not has_debug:
+        print('no debugging info found in {}'.format(k))
+        continue
 
-        print('loading symbols for {}'.format(k))
-        command(cmd, confirm=False, to_string=True)
+      print('loading symbols for {}'.format(k))
+      command(cmd, confirm=False, to_string=True)
 
 
 LoadSymbolFiles()
